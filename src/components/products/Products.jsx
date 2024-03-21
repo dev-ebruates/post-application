dfc3b02e42b43162353b5ab93a629b7f4516b37c
ebru <devatesebru@gmail.com>
2024-03-21 14:43:06 +0300

    Products Component

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,206 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="products-wrapper grid gap-4 grid-cols-card">
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+        <div className="product-img">
+          <img
+            src="https://www.agfresh.com.tr/public/uploads/elma.webp"
+            alt=""
+            className="w-full object-cover h-28 border-b"
+          />
+        </div>
+        <div className="product-info flex flex-col p-3">
+          <span className="font-bold">Elma</span>
+          <span>12₺</span>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Products;
